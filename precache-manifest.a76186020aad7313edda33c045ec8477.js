5a436bc92d3ed0cca85475c6182e70f4042cd0fe
xming13 <tanweiseng13@gmail.com>
2020-03-01 12:52:07 +0800

    Updates

---
+++ precache-manifest.a76186020aad7313edda33c045ec8477.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "517773ba6293ed4a92c37ba6e1fdc0be",
+    "url": "/solitaire-calendar/index.html"
+  },
+  {
+    "revision": "6e4431ffc8fabd943b28",
+    "url": "/solitaire-calendar/static/css/main.349423a8.chunk.css"
+  },
+  {
+    "revision": "2581002ba9fb75f6bcae",
+    "url": "/solitaire-calendar/static/js/2.ec1a6315.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/solitaire-calendar/static/js/2.ec1a6315.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e4431ffc8fabd943b28",
+    "url": "/solitaire-calendar/static/js/main.81f932da.chunk.js"
+  },
+  {
+    "revision": "a2259a283056eb6c358c",
+    "url": "/solitaire-calendar/static/js/runtime-main.16d0735d.js"
+  }
+]);
